2438969fa8164815e598b7c91fa37b0deaa65905
Abilogos <dev.abi.log@gmail.com>
2023-12-23 23:25:04 +0300

    speed gears has been added

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $(".auto-scroll-container").each(function () {
-    autoScroll(this, {dir:'right'});
+    autoScroll(this, {dir:'right', gear:2});
 });
 });
 
@@ -9,11 +9,29 @@ function autoScroll(obj, options) {
     $(obj).css("flex-wrap", "nowrap");
     $(obj).css("display", "flex");
 
+    // speed gears (21 is the speed of light :) )
+    const speedGears = [
+        [4000, 1],
+        [1000, 1],
+
+        [250, 1],
+        [62, 1],
+
+        [15, 1],
+        [15, 4],
+
+        [15, 16],
+        [15, 64],
+
+    ];
+    let gear = Number.isInteger(options.gear) && options.gear >=0 && options.gear < 8 ? options.gear : 4;
+    let curSpeedParams = speedGears[gear];
+
     let scrollDirMul = options.dir == 'left' ? -1 : 1;//defult to right
 
     setInterval(function () {
-        $(obj).scrollLeft($(obj).scrollLeft() + (scrollDirMul* 1) );
-    }, 30);
+        $(obj).scrollLeft($(obj).scrollLeft() + (scrollDirMul* curSpeedParams[1]) );
+    }, curSpeedParams[0]);
 
     if (window.IntersectionObserver !== undefined) {
         var observer = new IntersectionObserver(
