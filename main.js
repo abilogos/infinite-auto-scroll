c8000fdedb39914579ab1baf7dc8b9ecb868ec5c
Abilogos <dev.abi.log@gmail.com>
2023-12-23 22:00:24 +0300

    add dir option

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
 $(".auto-scroll-container").each(function () {
-    autoScroll(this);
+    autoScroll(this, {dir:'right'});
 });
 });
 
-function autoScroll(obj) {
+function autoScroll(obj, options) {
     $(obj).css("overflow","auto");
-    $(obj).css("flex-wrap", "nowrap")
-    $(obj).css("display", "flex")
+    $(obj).css("flex-wrap", "nowrap");
+    $(obj).css("display", "flex");
+
+    let scrollDirMul = options.dir == 'left' ? -1 : 1;//defult to right
 
     setInterval(function () {
-        $(obj).scrollLeft($(obj).scrollLeft() - 1);
+        $(obj).scrollLeft($(obj).scrollLeft() + (scrollDirMul* 1) );
     }, 30);
 
     if (window.IntersectionObserver !== undefined) {
@@ -20,7 +22,7 @@ function autoScroll(obj) {
                 if (element.isIntersecting === false) {
                     $(obj).append($(element.target));
                     let width = element.boundingClientRect.width;
-                    $(obj).scrollLeft($(obj).scrollLeft() + width);
+                    $(obj).scrollLeft($(obj).scrollLeft() + (-1*scrollDirMul* width ) );
                 }
             },
             {
